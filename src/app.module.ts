12ba26969d250eeee2e7784ea04c546fd3aec091
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-08-03 18:24:55 +0500

    database configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot(),
+        TypeOrmModule.forRootAsync({
+            imports: [ ConfigModule ],
+            inject: [ ConfigService ],
+            useFactory: async (configService: ConfigService) =>
+            {
+                const isProduction = configService.get('NODE_ENV') === 'production';
+
+                return {
+                    ssl: isProduction,
+                    extra: {
+                        ssl: isProduction ? { rejectUnauthorized: false } : null
+                    },
+                    type: 'postgres',
+                    autoLoadEntities: true,
+                    synchronize: !isProduction,
+                    host: configService.get('DATABASE_HOST'),
+                    port: configService.get('DATABASE_PORT'),
+                    database: configService.get('DATABASE_NAME'),
+                    username: configService.get('DATABASE_USERNAME'),
+                    password: configService.get('DATABASE_PASSWORD'),
+                };
+            },
+        }),
+    ],
 })
-export class AppModule {}
+export class AppModule { }
