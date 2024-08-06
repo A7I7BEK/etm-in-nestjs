36178dc23fa1b76f774f964b44978d8028cde1e4
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-08-06 12:41:06 +0500

    user fix

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,5 +1,6 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Role } from '../enums/role.enum';
+import { Organization } from 'src/organizations/entities/organization.entity';
 
 @Entity()
 export class User
@@ -8,11 +9,26 @@ export class User
     id: number;
 
     @Column({ unique: true })
+    userName: string;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column({ unique: true })
     email: string;
+
+    @Column()
+    phoneNumber: string;
 
     @Column()
     password: string;
 
     @Column({ enum: Role, default: Role.Regular })
     role: Role;
+
+    @ManyToOne(type => Organization, organization => organization.user)
+    organization: Organization;
 }
