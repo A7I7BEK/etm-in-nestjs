83e03466427d3d325dfc29249cf145fe9d7da833
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-08-11 16:32:18 +0500

    ValidationPipe fix

--- src/main.ts
+++ src/main.ts
@@ -15,9 +15,6 @@ async function bootstrap()
     app.useGlobalPipes(new ValidationPipe({
         whitelist: true,
         transform: true,
-        transformOptions: {
-            enableImplicitConversion: true
-        }
     }));
 
     // Swagger setup
