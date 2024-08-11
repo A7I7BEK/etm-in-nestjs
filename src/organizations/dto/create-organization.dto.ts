e2fbbfdc6b5a104f77e8422e1eb185bfd194a56c
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-08-11 13:58:28 +0500

    organization fix

--- src/organizations/dto/create-organization.dto.ts
+++ src/organizations/dto/create-organization.dto.ts
@@ -1,9 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateOrganizationDto
 {
     @ApiProperty({ example: 'Google' })
+    @IsString()
+    @IsNotEmpty()
     name: string;
 
     @ApiProperty({ example: 'aaa@aaa.aaa' })
