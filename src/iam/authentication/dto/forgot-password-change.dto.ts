5ec164fdc26451c64e6a928058c571baa0a24005
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-08-12 18:26:56 +0500

    dto refactoring

--- src/iam/authentication/dto/forgot-password-change.dto.ts
+++ src/iam/authentication/dto/forgot-password-change.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumberString, IsUUID, MinLength } from 'class-validator';
+import { IsUUID, MinLength } from 'class-validator';
 
 export class ForgotPasswordChangeDto
 {
--- src/iam/authentication/dto/refresh-token.dto.ts
+++ src/iam/authentication/dto/refresh-token.dto.ts
@@ -1,7 +1,8 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class RefreshTokenDto
 {
+    @IsString()
     @IsNotEmpty()
     refreshToken: string;
 }
