2b4e4c076b1a76a0ce2af0345dede3bf9d9f40d7
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-08-12 18:26:13 +0500

    otp entity

---
+++ src/iam/authentication/entities/one-time-password.entity.ts
@@ -0,0 +1,25 @@
+import { User } from 'src/users/entities/user.entity';
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn, Relation } from 'typeorm';
+
+@Entity()
+export class OneTimePassword
+{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column('uuid')
+    otpId: string;
+
+    @Column()
+    otpCode: number;
+
+    @Column()
+    expireTime: Date;
+
+    @Column({ default: false })
+    used: boolean;
+
+    @OneToOne(type => User)
+    @JoinColumn()
+    user: Relation<User>;
+}
