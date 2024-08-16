061f91e37a5ab60f4f27d8f2ce680a60dc07d898
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-08-16 13:23:29 +0500

    iam module fix

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -10,15 +10,16 @@ import { User } from 'src/users/entities/user.entity';
 import { AuthenticationController } from './authentication/authentication.controller';
 import { AuthenticationService } from './authentication/authentication.service';
 import { ForgotPassword } from './authentication/entities/forgot-password.entity';
+import { OneTimePasswordParent } from './authentication/entities/one-time-password-parent.entity';
 import { OneTimePassword } from './authentication/entities/one-time-password.entity';
 import { AccessTokenGuard } from './authentication/guards/access-token.guard';
 import { AuthenticationGuard } from './authentication/guards/authentication.guard';
+import { OneTimePasswordService } from './authentication/one-time-password.service';
 import { RefreshTokenIdsStorage } from './authentication/refresh-token-ids.storage';
 import { UserSerializer } from './authentication/serializers/user-serializer';
 import { RolesGuard } from './authorization/guards/roles.guard';
 import { BcryptService } from './hashing/bcrypt.service';
 import { HashingService } from './hashing/hashing.service';
-import { OneTimePasswordService } from './authentication/one-time-password.service';
 
 @Module({
     imports: [
@@ -26,12 +27,13 @@ import { OneTimePasswordService } from './authentication/one-time-password.servi
             Organization,
             User,
             Employee,
+            OneTimePasswordParent,
             OneTimePassword,
             ForgotPassword,
         ]),
         JwtModule.registerAsync({
             useFactory: async () => ({
-                secret: appConfig().jwt.secret,
+                secret: appConfig().jwt.accessTokenSecret,
                 signOptions: {
                     expiresIn: appConfig().jwt.accessTokenTtl,
                 }
