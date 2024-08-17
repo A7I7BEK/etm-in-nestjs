6bd9ed91675faa58d28f327cd49fed8efdced480
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-08-18 01:33:11 +0500

    language is created

---
+++ src/users/language/language.constant.ts
@@ -0,0 +1,16 @@
+import { ILanguage } from './language.interface';
+
+export const LANGUAGE_LIST: ILanguage[] = [
+    {
+        code: 'en',
+        name: 'English',
+    },
+    {
+        code: 'uz',
+        name: 'Uzbek',
+    },
+    {
+        code: 'ru',
+        name: 'Russian',
+    },
+];
---
+++ src/users/language/language.interface.ts
@@ -0,0 +1,12 @@
+export interface ILanguage
+{
+    /**
+     * The code of Language
+     */
+    code: string;
+
+    /**
+     * The name of Language
+     */
+    name: string;
+}
