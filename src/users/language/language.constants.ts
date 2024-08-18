ca71e1cc0aa3ba663ccf407c0b1a6b5398991d4e
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-08-18 21:08:34 +0500

    LANGUAGE_LIST removed from lang constants

--- src/users/language/language.constants.ts
+++ src/users/language/language.constants.ts
@@ -1,20 +1,7 @@
 import { Language } from './language.enum';
 import { ILanguage } from './language.interface';
 
-export const LANGUAGE_LIST: ILanguage[] = [
-    {
-        code: Language.English,
-        name: 'English',
-    },
-    {
-        code: Language.Uzbek,
-        name: 'Uzbek',
-    },
-    {
-        code: Language.Russian,
-        name: 'Russian',
-    },
-];
-
-
-export const LANGUAGE_DEFAULT: ILanguage = LANGUAGE_LIST.find(lang => lang.code = Language.English);
+export const LANGUAGE_DEFAULT: ILanguage = {
+    code: Language.English,
+    name: 'English',
+};
