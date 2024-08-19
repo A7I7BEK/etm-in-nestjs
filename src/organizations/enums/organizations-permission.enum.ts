da7ac9e6e1e50fb94c43f09d824cff18a4cd2bb2
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-08-19 20:37:52 +0500

    organization permissions created and embedded

---
+++ src/organizations/enums/organizations-permission.enum.ts
@@ -0,0 +1,7 @@
+export enum OrganizationsPermission
+{
+    OrganizationCreate = 'ORGANIZATION_CREATE',
+    OrganizationRead = 'ORGANIZATION_READ',
+    OrganizationUpdate = 'ORGANIZATION_UPDATE',
+    OrganizationDelete = 'ORGANIZATION_DELETE',
+}
--- src/organizations/organizations.controller.ts
+++ src/organizations/organizations.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Permissions } from 'src/iam/authorization/decorators/permissions.decorator';
 import { CreateOrganizationDto } from './dto/create-organization.dto';
 import { UpdateOrganizationDto } from './dto/update-organization.dto';
+import { OrganizationsPermission } from './enums/organizations-permission.enum';
 import { OrganizationsService } from './organizations.service';
 
 @ApiTags('organizations')
@@ -11,30 +13,35 @@ export class OrganizationsController
     constructor (private readonly organizationsService: OrganizationsService) { }
 
     @Post()
+    @Permissions(OrganizationsPermission.OrganizationCreate)
     create(@Body() createOrganizationDto: CreateOrganizationDto)
     {
         return this.organizationsService.create(createOrganizationDto);
     }
 
     @Get()
+    @Permissions(OrganizationsPermission.OrganizationRead)
     findAll()
     {
         return this.organizationsService.findAll();
     }
 
     @Get(':id')
+    @Permissions(OrganizationsPermission.OrganizationRead)
     findOne(@Param('id') id: string)
     {
         return this.organizationsService.findOne(+id);
     }
 
     @Put(':id')
+    @Permissions(OrganizationsPermission.OrganizationUpdate)
     update(@Param('id') id: string, @Body() updateOrganizationDto: UpdateOrganizationDto)
     {
         return this.organizationsService.update(+id, updateOrganizationDto);
     }
 
     @Delete(':id')
+    @Permissions(OrganizationsPermission.OrganizationDelete)
     remove(@Param('id') id: string)
     {
         return this.organizationsService.remove(+id);
