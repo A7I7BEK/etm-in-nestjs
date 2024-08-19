2b0cbc73e9d1d96fc44256cd038ced477160bb81
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-08-19 20:37:27 +0500

    employee permission created and embedded

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,8 +1,10 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Permissions } from 'src/iam/authorization/decorators/permissions.decorator';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
 import { EmployeesService } from './employees.service';
+import { EmployeesPermission } from './enums/employees-permission.enum';
 
 @ApiTags('employees')
 @Controller('employees')
@@ -11,30 +13,35 @@ export class EmployeesController
     constructor (private readonly employeesService: EmployeesService) { }
 
     @Post()
+    @Permissions(EmployeesPermission.EmployeeCreate)
     create(@Body() createEmployeeDto: CreateEmployeeDto)
     {
         return this.employeesService.create(createEmployeeDto);
     }
 
     @Get()
+    @Permissions(EmployeesPermission.EmployeeRead)
     findAll()
     {
         return this.employeesService.findAll();
     }
 
     @Get(':id')
+    @Permissions(EmployeesPermission.EmployeeRead)
     findOne(@Param('id') id: string)
     {
         return this.employeesService.findOne(+id);
     }
 
     @Put(':id')
+    @Permissions(EmployeesPermission.EmployeeUpdate)
     update(@Param('id') id: string, @Body() updateEmployeeDto: UpdateEmployeeDto)
     {
         return this.employeesService.update(+id, updateEmployeeDto);
     }
 
     @Delete(':id')
+    @Permissions(EmployeesPermission.EmployeeDelete)
     remove(@Param('id') id: string)
     {
         return this.employeesService.remove(+id);
---
+++ src/employees/enums/employees-permission.enum.ts
@@ -0,0 +1,8 @@
+export enum EmployeesPermission
+{
+    EmployeeCreate = 'EMPLOYEE_CREATE',
+    EmployeeRead = 'EMPLOYEE_READ',
+    EmployeeUpdate = 'EMPLOYEE_UPDATE',
+    EmployeeDelete = 'EMPLOYEE_DELETE',
+    EmployeePasswordChange = 'EMPLOYEE_PASSWORD_CHANGE',
+}
