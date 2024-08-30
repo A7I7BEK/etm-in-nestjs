0a741224c0b0ad6780f49843876d655784d9bfa0
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-08-30 23:02:34 +0500

    mail service exception handle

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,5 +1,5 @@
 import { MailerService } from '@nestjs-modules/mailer';
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import appConfig from 'src/common/config/app.config';
 import { User } from 'src/users/entities/user.entity';
 
@@ -8,16 +8,23 @@ export class MailService
 {
     constructor (private readonly mailerService: MailerService) { }
 
-    sendOtpCode(user: User, code: string)
+    async sendOtpCode(user: User, code: string)
     {
-        return this.mailerService.sendMail({
-            to: user.email,
-            subject: `${code} is your ${appConfig().application.name} verification code`,
-            template: './confirmation',
-            context: {
-                name: user.userName,
-                code,
-            },
-        });
+        try
+        {
+            await this.mailerService.sendMail({
+                to: user.email,
+                subject: `${code} is your ${appConfig().application.name} verification code`,
+                template: './confirmation',
+                context: {
+                    name: user.userName,
+                    code,
+                },
+            });
+        }
+        catch (error)
+        {
+            throw new HttpException(error.response, error.responseCode);
+        }
     }
 }
