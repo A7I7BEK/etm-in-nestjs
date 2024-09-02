b4f6b4375da7345a89faaf04d468e16506965d68
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-09-02 19:41:35 +0500

    permission constants: roles permission added

--- src/iam/authorization/permission.constants.ts
+++ src/iam/authorization/permission.constants.ts
@@ -1,10 +1,12 @@
 import { EmployeesPermission } from 'src/employees/enums/employees-permission.enum';
 import { OrganizationsPermission } from 'src/organizations/enums/organizations-permission.enum';
+import { RolesPermission } from 'src/roles/enums/roles-permission.enum';
 
 
 export type PermissionType = (
     EmployeesPermission
     | OrganizationsPermission
+    | RolesPermission
 );
 
 export const PERMISSION_TYPE_KEY = 'PERMISSION_TYPE_KEY';
@@ -12,4 +14,5 @@ export const PERMISSION_TYPE_KEY = 'PERMISSION_TYPE_KEY';
 export const permissionList = [
     ...Object.values(EmployeesPermission),
     ...Object.values(OrganizationsPermission),
+    ...Object.values(RolesPermission),
 ];
