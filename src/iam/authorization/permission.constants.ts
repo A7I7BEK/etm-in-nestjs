33e2ddf696972d0d3d0737f05a6130eb4be12ef3
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-09-05 15:11:40 +0500

    permission.constants.ts updated

--- src/iam/authorization/permission.constants.ts
+++ src/iam/authorization/permission.constants.ts
@@ -1,18 +1,39 @@
+import { CheckListGroupsPermission } from 'src/check-list-groups/enums/check-list-groups-permission.enum';
 import { EmployeesPermission } from 'src/employees/enums/employees-permission.enum';
+import { GroupsPermission } from 'src/groups/enums/groups-permission.enum';
 import { OrganizationsPermission } from 'src/organizations/enums/organizations-permission.enum';
+import { PermissionsPermission } from 'src/permissions/enums/permissions-permission.enum';
+import { ProjectsPermission } from 'src/projects/enums/projects-permission.enum';
+import { ReportsPermission } from 'src/reports/enums/reports-permission.enum';
 import { RolesPermission } from 'src/roles/enums/roles-permission.enum';
+import { TasksPermission } from 'src/tasks/enums/tasks-permission.enum';
+import { UsersPermission } from 'src/users/enums/users-permission.enum';
 
 
 export type PermissionType = (
-    EmployeesPermission
-    | OrganizationsPermission
+    OrganizationsPermission
+    | PermissionsPermission
     | RolesPermission
+    | UsersPermission
+    | EmployeesPermission
+    | GroupsPermission
+    | ProjectsPermission
+    | TasksPermission
+    | CheckListGroupsPermission
+    | ReportsPermission
 );
 
 export const PERMISSION_TYPE_KEY = 'PERMISSION_TYPE_KEY';
 
 export const permissionList = [
-    ...Object.values(EmployeesPermission),
     ...Object.values(OrganizationsPermission),
+    ...Object.values(PermissionsPermission),
     ...Object.values(RolesPermission),
+    ...Object.values(UsersPermission),
+    ...Object.values(EmployeesPermission),
+    ...Object.values(GroupsPermission),
+    ...Object.values(ProjectsPermission),
+    ...Object.values(TasksPermission),
+    ...Object.values(CheckListGroupsPermission),
+    ...Object.values(ReportsPermission),
 ];
