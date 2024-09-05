4141db80069238e201bfa210529e766a5967cda5
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-09-05 14:46:53 +0500

    projects CRUD created

---
+++ src/projects/enums/projects-permission.enum.ts
@@ -0,0 +1,17 @@
+export enum ProjectsPermission
+{
+    Create = 'PROJECT_CREATE',
+    Read = 'PROJECT_READ',
+    Update = 'PROJECT_UPDATE',
+    Delete = 'PROJECT_DELETE',
+
+    ReadDetails = 'PROJECT_READ_DETAILS',
+    ChangePhoto = 'PROJECT_CHANGE_PHOTO',
+
+    ColumnCreate = 'PROJECT_COLUMN_CREATE',
+    ColumnRead = 'PROJECT_COLUMN_READ',
+    ColumnUpdate = 'PROJECT_COLUMN_UPDATE',
+
+    MemberCreate = 'PROJECT_MEMBER_CREATE',
+    MemberRead = 'PROJECT_MEMBER_READ',
+}
