f0e41145db4f3bb2a2f044e52765f83131a2119c
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-09-23 23:18:00 +0500

    system admin creation is ready

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,23 @@ import { AppService } from './app.service';
 import { CheckListGroupsModule } from './check-list-groups/check-list-groups.module';
 import appConfig from './common/config/app.config';
 import { EmployeesModule } from './employees/employees.module';
+import { Employee } from './employees/entities/employee.entity';
 import { GroupsModule } from './groups/groups.module';
+import { BcryptService } from './iam/hashing/bcrypt.service';
+import { HashingService } from './iam/hashing/hashing.service';
 import { IamModule } from './iam/iam.module';
 import { MailModule } from './mail/mail.module';
+import { Organization } from './organizations/entities/organization.entity';
 import { OrganizationsModule } from './organizations/organizations.module';
 import { Permission } from './permissions/entities/permission.entity';
 import { PermissionsModule } from './permissions/permissions.module';
 import { ProjectsModule } from './projects/projects.module';
 import { ReportsModule } from './reports/reports.module';
 import { ResourceModule } from './resource/resource.module';
+import { Role } from './roles/entities/role.entity';
 import { RolesModule } from './roles/roles.module';
 import { TasksModule } from './tasks/tasks.module';
+import { User } from './users/entities/user.entity';
 import { UsersModule } from './users/users.module';
 
 @Module({
@@ -38,7 +44,13 @@ import { UsersModule } from './users/users.module';
                 };
             },
         }),
-        TypeOrmModule.forFeature([ Permission ]),
+        TypeOrmModule.forFeature([
+            Organization,
+            User,
+            Employee,
+            Role,
+            Permission,
+        ]),
         IamModule,
         UsersModule,
         OrganizationsModule,
@@ -53,6 +65,12 @@ import { UsersModule } from './users/users.module';
         CheckListGroupsModule,
         ReportsModule,
     ],
-    providers: [ AppService ],
+    providers: [
+        AppService,
+        {
+            provide: HashingService,
+            useClass: BcryptService,
+        },
+    ],
 })
 export class AppModule { }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,20 +1,36 @@
-import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
+import { ConflictException, Injectable, OnApplicationBootstrap } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import appConfig from './common/config/app.config';
+import { Employee } from './employees/entities/employee.entity';
 import { permissionList } from './iam/authorization/permission.constants';
+import { HashingService } from './iam/hashing/hashing.service';
+import { Organization } from './organizations/entities/organization.entity';
 import { Permission } from './permissions/entities/permission.entity';
+import { Role } from './roles/entities/role.entity';
+import { User } from './users/entities/user.entity';
 
 @Injectable()
 export class AppService implements OnApplicationBootstrap
 {
     constructor (
+        @InjectRepository(Organization)
+        private readonly organizationsRepository: Repository<Organization>,
+        @InjectRepository(User)
+        private readonly usersRepository: Repository<User>,
+        @InjectRepository(Employee)
+        private readonly employeesRepository: Repository<Employee>,
+        @InjectRepository(Role)
+        private readonly rolesRepository: Repository<Role>,
         @InjectRepository(Permission)
         private readonly permissionsRepository: Repository<Permission>,
+        private readonly hashingService: HashingService,
     ) { }
 
     async onApplicationBootstrap()
     {
         await this.clearAndInsertPermissions();
+        await this.createSystemAdmin();
     }
 
     async clearAndInsertPermissions()
@@ -24,19 +40,56 @@ export class AppService implements OnApplicationBootstrap
             codeName: perm,
         }));
 
+        // await this.permissionsRepository.delete({});
+        await this.permissionsRepository
+            .createQueryBuilder()
+            .insert()
+            .values(permissions)
+            .orIgnore()
+            .execute();
+    }
+
+    async createSystemAdmin()
+    {
         try
         {
-            await this.permissionsRepository.delete({});
-            await this.permissionsRepository
-                .createQueryBuilder()
-                .insert()
-                .values(permissions)
-                .orIgnore()
-                .execute();
+            const organizationExists = await this.organizationsRepository.existsBy({ name: appConfig().admin.orgName });
+            if (organizationExists)
+            {
+                throw new ConflictException('Organization already exists');
+            }
+
+            const organizationEntity = new Organization();
+            organizationEntity.name = appConfig().admin.orgName;
+            await this.organizationsRepository.save(organizationEntity);
+
+            const roleEntity = new Role();
+            roleEntity.roleName = appConfig().admin.roleName.toLocaleLowerCase();
+            roleEntity.codeName = appConfig().admin.roleName;
+            roleEntity.permissions = await this.permissionsRepository.find();
+            roleEntity.organization = organizationEntity;
+            await this.rolesRepository.save(roleEntity);
+
+            const userEntity = new User();
+            userEntity.systemAdmin = true;
+            userEntity.active = true;
+            userEntity.userName = appConfig().admin.username;
+            userEntity.password = await this.hashingService.hash(appConfig().admin.password);
+            userEntity.email = appConfig().admin.email;
+            userEntity.phoneNumber = appConfig().admin.phoneNumber;
+            userEntity.organization = organizationEntity;
+            userEntity.roles = [ roleEntity ];
+            await this.usersRepository.save(userEntity);
+
+            const employeeEntity = new Employee();
+            employeeEntity.firstName = appConfig().admin.firstName;
+            employeeEntity.lastName = appConfig().admin.lastName;
+            employeeEntity.user = userEntity;
+            await this.employeesRepository.save(employeeEntity);
         }
         catch (error)
         {
-            console.log('PERMISSION_INSERT', error);
+            console.log('SYSTEM_ADMIN_CREATION', error);
         }
     }
 }
