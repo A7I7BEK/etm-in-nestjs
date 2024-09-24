ca7fcb641a6e179f934984a8f93d8bc8a9d90126
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-09-24 18:51:15 +0500

    CreateRoleDto is ready

--- src/roles/dto/create-role.dto.ts
+++ src/roles/dto/create-role.dto.ts
@@ -1,1 +1,19 @@
-export class CreateRoleDto {}
+import { IsNotEmpty, IsPositive, IsString, ValidateNested } from 'class-validator';
+import { ObjectIdDto } from 'src/common/dto/object-id.dto';
+
+export class CreateRoleDto
+{
+    @IsString()
+    @IsNotEmpty()
+    roleName: string;
+
+    @IsString()
+    @IsNotEmpty()
+    codeName: string;
+
+    @ValidateNested()
+    permissions: ObjectIdDto[];
+
+    @IsPositive()
+    organizationId: number;
+}
