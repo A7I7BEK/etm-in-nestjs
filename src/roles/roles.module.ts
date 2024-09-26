fd082cba3c16611a13f0d6cb36576243a7cc8736
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-09-26 18:02:30 +0500

    role create is ready

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Organization } from 'src/organizations/entities/organization.entity';
+import { Permission } from 'src/permissions/entities/permission.entity';
 import { Role } from './entities/role.entity';
 import { RolesController } from './roles.controller';
 import { RolesService } from './roles.service';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([ Role ]),
+        TypeOrmModule.forFeature([
+            Role,
+            Permission,
+            Organization,
+        ]),
     ],
     controllers: [ RolesController ],
     providers: [ RolesService ],
--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Organization } from 'src/organizations/entities/organization.entity';
+import { Permission } from 'src/permissions/entities/permission.entity';
+import { In, Repository } from 'typeorm';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
 import { Role } from './entities/role.entity';
@@ -11,13 +13,25 @@ export class RolesService
     constructor (
         @InjectRepository(Role)
         private readonly rolesRepository: Repository<Role>,
+        @InjectRepository(Permission)
+        private readonly permissionsRepository: Repository<Permission>,
+        @InjectRepository(Organization)
+        private readonly organizationsRepository: Repository<Organization>,
     ) { }
 
-    create(createRoleDto: CreateRoleDto)
+    async create(createRoleDto: CreateRoleDto)
     {
-        const entity = this.rolesRepository.create({
-            ...createRoleDto
-        });
+        const organizationFound = await this.organizationsRepository.findOneByOrFail({ id: createRoleDto.organizationId });
+
+        const permissionIds = createRoleDto.permissions.map(x => x.id);
+        const permissionsFound = await this.permissionsRepository.findBy({ id: In(permissionIds) });
+
+        const entity = new Role();
+        entity.roleName = createRoleDto.roleName;
+        entity.codeName = createRoleDto.codeName;
+        entity.organization = organizationFound;
+        entity.permissions = permissionsFound;
+
         return this.rolesRepository.save(entity);
     }
 
