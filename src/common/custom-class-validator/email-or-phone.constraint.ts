09572d7796ff31b7f349e4bb272657b0fcf7838a
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-10-02 19:55:36 +0500

    EmailOrPhoneConstraint custom class validator is created

---
+++ src/common/custom-class-validator/email-or-phone.constraint.ts
@@ -0,0 +1,28 @@
+import { ValidationArguments, ValidatorConstraint, ValidatorConstraintInterface, isEmail, isNumberString, isPhoneNumber } from 'class-validator';
+
+@ValidatorConstraint({ name: 'EmailOrPhone', async: false })
+export class EmailOrPhoneConstraint implements ValidatorConstraintInterface
+{
+    validate(value: unknown, args?: ValidationArguments): boolean
+    {
+        if (typeof value === 'string')
+        {
+            if (isEmail(value))
+            {
+                return true;
+            }
+
+            if (isPhoneNumber(value, 'UZ') && isNumberString(value, { no_symbols: true }))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    defaultMessage(args?: ValidationArguments)
+    {
+        return `${args.property} must be an email or phone number`;
+    }
+}
