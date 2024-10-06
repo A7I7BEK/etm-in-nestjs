17af0c2ab33ad85c6b090b164d22bb242ece2232
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-10-06 12:33:29 +0500

    filename-generator.ts small fix

--- src/resource/utils/filename-generator.ts
+++ src/resource/utils/filename-generator.ts
@@ -2,7 +2,7 @@ import { extname } from 'path';
 
 export function FilenameGenerator(file: Express.Multer.File)
 {
-    const dateTimeSuffix = new Date().toISOString().replace(/[-:]/g, '').replace('T', '_').split('.')[ 0 ];
+    const dateTimeSuffix = new Date().toISOString().replace(/[-:.Z]/g, '').replace('T', '_');
     const randomSuffix = Math.round(Math.random() * 1E9);
     const ext = extname(file.originalname);
     const filename = file.fieldname + '_' + dateTimeSuffix + '_' + randomSuffix + ext;
