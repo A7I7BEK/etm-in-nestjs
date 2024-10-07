61756d25a984a6f78d3801f71ab734e422fd26b0
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-10-07 18:16:25 +0500

    diskStorage method of saving files is removed

--- src/resource/resource.module.ts
+++ src/resource/resource.module.ts
@@ -1,23 +1,13 @@
 import { BadRequestException, Module } from '@nestjs/common';
 import { MulterModule } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
 import { ResourceController } from './resource.controller';
 import { ResourceService } from './resource.service';
-import { filenameGenerator } from './utils/filename-generator';
-import { ALLOWED_MIME_TYPES, DESTINATION, MAX_FILE_COUNT, MAX_FILE_SIZE } from './utils/resource.constants';
+import { ALLOWED_MIME_TYPES, MAX_FILE_COUNT, MAX_FILE_SIZE } from './utils/resource.constants';
 
 @Module({
     imports: [
         MulterModule.registerAsync({
             useFactory: async () => ({
-                storage: diskStorage({
-                    destination: DESTINATION,
-                    filename(req, file, callback)
-                    {
-                        const filename = filenameGenerator(file);
-                        callback(null, filename);
-                    },
-                }),
                 fileFilter: (req, file, callback) =>
                 {
                     if (ALLOWED_MIME_TYPES.includes(file.mimetype))
