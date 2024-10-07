59b1b7ea66cb85ee98e21439f6aaab2506ee0d03
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-10-07 22:32:59 +0500

    upload file almost ready

--- src/resource/resource.service.ts
+++ src/resource/resource.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as sharp from 'sharp';
+import { MinDimensionDto } from './dto/min-dimension.dto';
+import { MIME_TYPE_IMAGES } from './utils/resource.constants';
+import { generateFilename, getDestination } from './utils/resource.utils';
 
 @Injectable()
 export class ResourceService
 {
-    uploadFile(file: Express.Multer.File)
+    async uploadFile(file: Express.Multer.File, minDimensionDto: MinDimensionDto)
     {
-        console.log(file);
-        return { message: 'File uploaded successfully' };
+
+        const filePath = this.generateFilePath(file);
+
+        if (MIME_TYPE_IMAGES.includes(file.mimetype) && minDimensionDto.minWidth && minDimensionDto.minHeight)
+        {
+            const resizedImageBuffer = await this.resizeImage(file);
+            return this.saveFile(filePath, resizedImageBuffer);
+        }
+
+        return this.saveFile(filePath, file.buffer);
     }
 
     uploadMultipleFiles(files: Express.Multer.File[])
@@ -23,5 +37,57 @@ export class ResourceService
     remove(id: number)
     {
         return `This action removes a #${id} resource`;
+    }
+
+
+    private generateFilePath(file: Express.Multer.File)
+    {
+        const destination = getDestination(file);
+
+        if (!fs.existsSync(destination))
+        {
+            fs.mkdirSync(destination, { recursive: true });
+        }
+
+        const filename = generateFilename(file);
+        const filePath = path.join(destination, filename);
+
+        return filePath;
+    }
+
+    private async resizeImage(file: Express.Multer.File)
+    {
+        return await sharp(file.buffer)
+            .resize(800, 800, {
+                fit: sharp.fit.cover,
+                position: sharp.strategy.entropy
+            })
+            .toBuffer();
+
+        // const croppedImage = await sharp(file.buffer)
+        //     .resize(200, 200) // Adjust the desired dimensions
+        //     .crop(sharp.gravity.center)
+        //     .toBuffer();
+
+        // const resizedImage = await sharp(file.buffer)
+        //     .resize(800, 600) // Set your desired dimensions
+        //     .toBuffer();
+    }
+
+    private async saveFile(filePath: string, fileBuffer: Buffer)
+    {
+        try
+        {
+            await fs.promises.writeFile(filePath, fileBuffer);
+
+            return {
+                message: 'File uploaded successfully',
+                filePath,
+            };
+        }
+        catch (error)
+        {
+            throw new Error('Failed to save file');
+        }
     }
 }
