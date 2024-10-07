605a741451404a1274bb5bce40f4093745e1b392
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-10-07 18:18:15 +0500

    destination for mime types added

--- src/resource/utils/resource.constants.ts
+++ src/resource/utils/resource.constants.ts
@@ -1,29 +1,31 @@
 
-export const DESTINATION = './uploads';
+export const DESTINATION_BASE = 'uploads';
+
+export const DESTINATION_IMAGE = DESTINATION_BASE + '/image';
+export const DESTINATION_AUDIO = DESTINATION_BASE + '/audio';
+export const DESTINATION_VIDEO = DESTINATION_BASE + '/video';
+export const DESTINATION_MICROSOFT_DOC = DESTINATION_BASE + '/ms-doc';
+export const DESTINATION_OTHER_DOC = DESTINATION_BASE + '/other-doc';
+export const DESTINATION_ARCHIVE = DESTINATION_BASE + '/archive';
+
 
 export const MAX_FILE_SIZE = 50 * 1024 * 1024; // 50MB
-
 export const MAX_FILE_COUNT = 10;
 
 
 export const MIME_TYPE_IMAGES = [ 'image/jpeg', 'image/png', 'image/gif', 'image/svg+xml', 'image/webp' ];
-
 export const MIME_TYPE_AUDIOS = [ 'audio/aac', 'audio/mpeg', 'audio/wav', 'audio/ogg', 'audio/webm' ];
-
 export const MIME_TYPE_VIDEOS = [ 'video/x-msvideo', 'video/mp4', 'video/mpeg', 'video/webm' ];
-
 export const MIME_TYPE_MICROSOFT_DOCS = [
     'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
     'application/vnd.ms-powerpoint', 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
     'application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
     'application/vnd.visio',
 ];
-
 export const MIME_TYPE_OTHER_DOCS = [
     'application/vnd.oasis.opendocument.presentation', 'application/vnd.oasis.opendocument.spreadsheet',
     'application/vnd.oasis.opendocument.text', 'application/pdf',
 ];
-
 export const MIME_TYPE_ARCHIVES = [ 'application/vnd.rar', 'application/zip', 'application/x-zip-compressed', 'application/x-7z-compressed' ];
 
 export const ALLOWED_MIME_TYPES = [
