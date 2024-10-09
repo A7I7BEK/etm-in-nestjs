f1a99204ee72222dcaa4c3197e2ab8c1b657d590
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-10-09 23:03:23 +0500

    Resource entity created

--- src/resource/entities/resource.entity.ts
+++ src/resource/entities/resource.entity.ts
@@ -1,1 +1,32 @@
-export class Resource {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Resource
+{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    url: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    filename: string;
+
+    @Column()
+    mimetype: string;
+
+    @Column()
+    size: number;
+
+    @Column()
+    sizeCalculated: string;
+
+    @Column()
+    createdAt: Date;
+
+    @Column()
+    now: Date;
+}
