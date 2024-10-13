f7dca5b70b81ec71228b72c618cbfb5bce566254
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-10-13 15:49:21 +0500

    UpdateEmployeeDto, UpdateUserDto are refactored

--- src/employees/dto/update-employee.dto.ts
+++ src/employees/dto/update-employee.dto.ts
@@ -1,35 +1,12 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { OmitType, PartialType } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { IsDateString, IsNotEmpty, IsOptional, IsString, ValidateNested } from 'class-validator';
-import { ObjectIdDto } from 'src/common/dto/object-id.dto';
+import { ValidateNested } from 'class-validator';
 import { UpdateUserDto } from 'src/users/dto/update-user.dto';
+import { CreateEmployeeDto } from './create-employee.dto';
 
-export class UpdateEmployeeDto
+// BINGO
+export class UpdateEmployeeDto extends PartialType(OmitType(CreateEmployeeDto, [ 'user' ]))
 {
-    @ApiProperty({ example: 'John' })
-    @IsString()
-    @IsNotEmpty()
-    firstName: string;
-
-    @ApiProperty({ example: 'Doe' })
-    @IsString()
-    @IsNotEmpty()
-    lastName: string;
-
-    @ApiProperty({ example: 'Tom' })
-    @IsString()
-    @IsNotEmpty()
-    @IsOptional()
-    middleName?: string;
-
-    @IsDateString()
-    @IsOptional()
-    birthDate?: Date;
-
-    @ValidateNested()
-    @Type(() => ObjectIdDto)
-    resourceFile?: ObjectIdDto;
-
     @ValidateNested()
     @Type(() => UpdateUserDto)
     user: UpdateUserDto;
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,22 +1,4 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, IsNumberString, IsPhoneNumber, IsPositive, IsString } from 'class-validator';
+import { OmitType, PartialType } from '@nestjs/swagger';
+import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto
-{
-    @IsPositive()
-    organizationId: number;
-
-    @ApiProperty({ example: 'johnny' })
-    @IsString()
-    @IsNotEmpty()
-    userName: string;
-
-    @ApiProperty({ example: 'aaa@aaa.aaa' })
-    @IsEmail()
-    email: string;
-
-    @ApiProperty({ example: '901234567' })
-    @IsNumberString({ no_symbols: true })
-    @IsPhoneNumber('UZ')
-    phoneNumber: string;
-}
+export class UpdateUserDto extends PartialType(OmitType(CreateUserDto, [ 'password' ])) { }
