35576aa8d7f7876fc54e7b3b21a65dfb8f8ee776
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-10-16 22:46:22 +0500

    users/me is done

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,10 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Permission } from 'src/iam/authorization/decorators/permission.decorator';
 import { ActiveUser } from 'src/iam/decorators/active-user.decorator';
 import { ActiveUserData } from 'src/iam/interfaces/active-user-data.interface';
+import { Repository } from 'typeorm';
 import { AttachRoleDto } from './dto/attach-role.dto';
 import { ChangeLanguageDto } from './dto/change-language.dto';
 import { User } from './entities/user.entity';
@@ -13,15 +15,21 @@ import { UsersService } from './users.service';
 @Controller('users')
 export class UsersController
 {
-    constructor (private readonly usersService: UsersService) { }
+    constructor (
+        @InjectRepository(User)
+        private readonly usersRepository: Repository<User>,
+        private readonly usersService: UsersService,
+    ) { }
 
     @Get('me')
-    getCurrentUser(@ActiveUser() user: ActiveUserData)
+    async getCurrentUser(@ActiveUser() activeUser: ActiveUserData)
     {
-        return {
-            user,
-            dbUser: this.usersService.findOne({ id: user.sub })
-        };
+        const entity = await this.usersRepository.findOne({
+            where: { id: activeUser.sub },
+            relations: { employee: true, organization: true, roles: true },
+        });
+
+        return this.returnModifiedEntity(entity, true, true);
     }
 
     @Post('attachRole')
@@ -40,18 +48,32 @@ export class UsersController
     }
 
 
-    private returnModifiedEntity(entity: User, organization?: boolean)
+    private returnModifiedEntity(entity: User, organization?: boolean, employee?: boolean)
     {
-        delete entity.password;
+        const { organization: org, employee: emp, ...entityRest } = entity;
+        const entityNew = { ...entityRest, ...{ userId: entity.id, systemAdmin: entity.marks.systemAdmin } };
 
-        const { organization: org, ...entityRest } = entity;
-        const entityNew = { ...entityRest };
+        delete entityNew.password;
+        delete entityNew.id;
+        delete entityNew.marks;
 
         if (organization)
         {
             Object.assign(entityNew, {
                 organizationId: org.id,
                 organizationName: org.name,
+            });
+        }
+
+        if (employee)
+        {
+            Object.assign(entityNew, {
+                id: emp.id,
+                firstName: emp.firstName,
+                lastName: emp.lastName,
+                middleName: emp.middleName,
+                birthDate: emp.birthDate,
+                photoUrl: emp.photoUrl,
             });
         }
 
