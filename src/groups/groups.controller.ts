4c1930fbfc52cc6093c29f684a6422b1b92dc2a3
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-10-17 16:56:22 +0500

    groups.controller.ts done temporarily

--- src/groups/groups.controller.ts
+++ src/groups/groups.controller.ts
@@ -1,38 +1,49 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { Permission } from 'src/iam/authorization/decorators/permission.decorator';
+import { ActiveUser } from 'src/iam/decorators/active-user.decorator';
+import { ActiveUserData } from 'src/iam/interfaces/active-user-data.interface';
 import { CreateGroupDto } from './dto/create-group.dto';
 import { UpdateGroupDto } from './dto/update-group.dto';
+import { GroupsPermission } from './enums/groups-permission.enum';
 import { GroupsService } from './groups.service';
 
+@ApiTags('groups')
 @Controller('groups')
 export class GroupsController
 {
     constructor (private readonly groupsService: GroupsService) { }
 
     @Post()
-    create(@Body() createGroupDto: CreateGroupDto)
+    @Permission(GroupsPermission.Create)
+    create(@Body() createGroupDto: CreateGroupDto, @ActiveUser() activeUser: ActiveUserData)
     {
         return this.groupsService.create(createGroupDto);
     }
 
     @Get()
+    @Permission(GroupsPermission.Read)
     findAll()
     {
         return this.groupsService.findAll();
     }
 
     @Get(':id')
+    @Permission(GroupsPermission.Read)
     findOne(@Param('id') id: string)
     {
         return this.groupsService.findOne(+id);
     }
 
     @Put(':id')
-    update(@Param('id') id: string, @Body() updateGroupDto: UpdateGroupDto)
+    @Permission(GroupsPermission.Update)
+    update(@Param('id') id: string, @Body() updateGroupDto: UpdateGroupDto, @ActiveUser() activeUser: ActiveUserData)
     {
         return this.groupsService.update(+id, updateGroupDto);
     }
 
     @Delete(':id')
+    @Permission(GroupsPermission.Delete)
     remove(@Param('id') id: string)
     {
         return this.groupsService.remove(+id);
