efb14a47cea5171812248babb270b12df9a2b6cf
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-10-17 16:57:04 +0500

    groups.module.ts done temporarily

--- src/groups/groups.module.ts
+++ src/groups/groups.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
-import { GroupsService } from './groups.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EmployeesModule } from 'src/employees/employees.module';
+import { Group } from './entities/group.entity';
 import { GroupsController } from './groups.controller';
+import { GroupsService } from './groups.service';
 
 @Module({
+    imports: [
+        TypeOrmModule.forFeature([ Group ]),
+        EmployeesModule,
+    ],
     controllers: [ GroupsController ],
     providers: [ GroupsService ],
 })
