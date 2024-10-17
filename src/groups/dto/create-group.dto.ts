2d5cd008ee6c021969756024ead89edfef29a772
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-10-17 16:32:18 +0500

    CreateGroupDto is done

--- src/groups/dto/create-group.dto.ts
+++ src/groups/dto/create-group.dto.ts
@@ -1,1 +1,22 @@
-export class CreateGroupDto { }
+import { Type } from 'class-transformer';
+import { ArrayMinSize, IsInt, IsNotEmpty, IsPositive, IsString, Min, ValidateNested } from 'class-validator';
+import { ObjectIdDto } from 'src/common/dto/object-id.dto';
+
+export class CreateGroupDto
+{
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @ValidateNested({ each: true })
+    @ArrayMinSize(1)
+    @Type(() => ObjectIdDto)
+    userIds: ObjectIdDto[];
+
+    @IsPositive()
+    leaderId: number;
+
+    @IsInt()
+    @Min(0)
+    organizationId: number;
+}
