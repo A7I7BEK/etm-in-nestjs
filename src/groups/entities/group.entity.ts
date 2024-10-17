6f570993f344fa69d940618cd0d9796c8023740b
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-10-17 16:21:49 +0500

    Group entity is done

--- src/groups/entities/group.entity.ts
+++ src/groups/entities/group.entity.ts
@@ -1,1 +1,22 @@
-export class Group { }
+import { Employee } from 'src/employees/entities/employee.entity';
+import { Organization } from 'src/organizations/entities/organization.entity';
+import { Column, JoinTable, ManyToMany, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Group
+{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @JoinTable()
+    @ManyToMany(type => Employee, { eager: true })
+    employees: Employee[];
+
+    @ManyToOne(type => Employee, { eager: true })
+    leader: Employee;
+
+    @ManyToOne(type => Organization)
+    organization: Organization;
+}
