f553955bee20e74b8a4fb19c6e66528304bef112
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-10-17 19:20:21 +0500

    GroupsModule is done

--- src/groups/groups.module.ts
+++ src/groups/groups.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EmployeesModule } from 'src/employees/employees.module';
+import { OrganizationsModule } from 'src/organizations/organizations.module';
+import { UsersModule } from 'src/users/users.module';
 import { Group } from './entities/group.entity';
 import { GroupsController } from './groups.controller';
 import { GroupsService } from './groups.service';
@@ -8,7 +10,9 @@ import { GroupsService } from './groups.service';
 @Module({
     imports: [
         TypeOrmModule.forFeature([ Group ]),
+        OrganizationsModule,
         EmployeesModule,
+        UsersModule,
     ],
     controllers: [ GroupsController ],
     providers: [ GroupsService ],
