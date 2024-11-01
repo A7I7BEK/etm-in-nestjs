604b324ea91459f21f00a91bac346664a5eb8fe3
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-11-01 21:25:12 +0500

    WrapResponseInterceptor changed

--- src/common/interceptors/wrap-response.interceptor.ts
+++ src/common/interceptors/wrap-response.interceptor.ts
@@ -1,11 +1,19 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
 import { Observable, map } from 'rxjs';
+import { Pagination } from '../pagination/pagination.class';
 
 @Injectable()
 export class WrapResponseInterceptor implements NestInterceptor
 {
     intercept(context: ExecutionContext, next: CallHandler): Observable<any>
     {
-        return next.handle().pipe(map(data => ({ data })));
+        return next.handle().pipe(map(data =>
+        {
+            if (data instanceof Pagination)
+            {
+                return data;
+            }
+            return { data };
+        }));
     }
 }
