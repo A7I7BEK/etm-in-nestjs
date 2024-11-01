e6f556be324d156332bbc83fca1b41ddae2a2257
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-11-02 01:03:10 +0500

    PageFilterDto refactoring

--- src/common/dto/page-filter.dto.ts
+++ src/common/dto/page-filter.dto.ts
@@ -1,27 +1,39 @@
+import { Type } from 'class-transformer';
 import { IsEnum, IsInt, IsOptional, IsString, Max, Min } from 'class-validator';
 import { Order } from '../pagination/order.enum';
 
+// BINGO
 export class PageFilterDto<T extends { id: number; }>
 {
-    @IsInt()
-    @Min(0)
+    // BINGO
     @IsOptional()
+    @Min(0)
+    @IsInt()
+    @Type(() => Number) // BINGO
     page?: number = 0;
 
-    @IsInt()
-    @Min(1)
-    @Max(100)
     @IsOptional()
+    @Max(100)
+    @Min(1)
+    @IsInt()
+    @Type(() => Number) // BINGO
     perPage?: number = 10;
 
     @IsOptional()
+    @IsString()
     sortBy?: keyof T = 'id';
 
-    @IsEnum(Order)
     @IsOptional()
+    @IsEnum(Order)
     sortDirection?: Order = Order.DESC;
 
-    @IsString()
     @IsOptional()
+    @IsString()
     allSearch?: string;
+
+
+    get skip(): number // BINGO
+    {
+        return this.page * this.perPage;
+    }
 }
