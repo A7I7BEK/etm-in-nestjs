9241dd47c63c82e967316b8f45b1494b32ccabbe
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-11-04 19:35:46 +0500

    OrderReverse created for temporary

--- src/common/pagination/order.enum.ts
+++ src/common/pagination/order.enum.ts
@@ -3,3 +3,12 @@ export enum Order
     ASC = 'asc',
     DESC = 'desc',
 }
+
+
+/**
+ * temporary for this project
+ */
+export const OrderReverse = {
+    [ Order.ASC ]: 'ASC',
+    [ Order.DESC ]: 'DESC',
+} as const;
