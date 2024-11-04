97f42746af5dbfe68aa8a78c819e367da0d76a6a
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-11-04 19:48:28 +0500

    organizationId added into RolePageFilterDto

--- src/roles/dto/role-page-filter.dto.ts
+++ src/roles/dto/role-page-filter.dto.ts
@@ -1,4 +1,5 @@
-import { IsEnum, IsOptional } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsEnum, IsInt, IsOptional, Min } from 'class-validator';
 import { PageFilterDto } from 'src/common/dto/page-filter.dto';
 import { RoleProperties } from '../enums/role-properties.enum';
 
@@ -8,4 +9,10 @@ export class RolePageFilterDto extends PageFilterDto<RoleProperties>
     @IsOptional()
     @IsEnum(RoleProperties)
     sortBy?: RoleProperties = RoleProperties.ID;
+
+    @IsOptional()
+    @Min(1)
+    @IsInt()
+    @Type(() => Number)
+    organizationId?: number;
 }
