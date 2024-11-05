e4c327fcaaee945e50a06a12bfd7ee77576ec9ce
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-11-05 20:53:20 +0500

    OrganizationsService findAllWithFilters method is ready

--- src/organizations/organizations.service.ts
+++ src/organizations/organizations.service.ts
@@ -1,8 +1,12 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import appConfig from 'src/common/config/app.config';
-import { FindOptionsRelations, FindOptionsWhere, Repository } from 'typeorm';
+import { OrderReverse } from 'src/common/pagination/order.enum';
+import { PaginationMeta } from 'src/common/pagination/pagination-meta.class';
+import { Pagination } from 'src/common/pagination/pagination.class';
+import { Brackets, FindManyOptions, FindOptionsRelations, FindOptionsWhere, Repository } from 'typeorm';
 import { CreateOrganizationDto } from './dto/create-organization.dto';
+import { OrganizationPageFilterDto } from './dto/organization-page-filter.dto';
 import { UpdateOrganizationDto } from './dto/update-organization.dto';
 import { Organization } from './entities/organization.entity';
 
@@ -22,9 +26,34 @@ export class OrganizationsService
         return this.organizationsRepository.save(entity);
     }
 
-    findAll()
+    findAll(options?: FindManyOptions<Organization>)
     {
-        return this.organizationsRepository.find();
+        return this.organizationsRepository.find(options);
+    }
+
+    async findAllWithFilters(pageFilterDto: OrganizationPageFilterDto)
+    {
+        const queryBuilder = this.organizationsRepository.createQueryBuilder('org');
+        queryBuilder.skip(pageFilterDto.skip);
+        queryBuilder.take(pageFilterDto.perPage);
+        queryBuilder.orderBy('org.' + pageFilterDto.sortBy, OrderReverse[ pageFilterDto.sortDirection ]);
+
+        if (pageFilterDto.allSearch)
+        {
+            queryBuilder.andWhere(
+                new Brackets((qb) =>
+                {
+                    qb.orWhere('org.name ILIKE :search', { search: `%${pageFilterDto.allSearch}%` });
+                    qb.orWhere('org.email ILIKE :search', { search: `%${pageFilterDto.allSearch}%` });
+                }),
+            );
+        }
+
+        const [ data, total ] = await queryBuilder.getManyAndCount();
+
+        const paginationMeta = new PaginationMeta(pageFilterDto.page, pageFilterDto.perPage, total);
+
+        return new Pagination<Organization>(data, paginationMeta);
     }
 
     async findOne(where: FindOptionsWhere<Organization>, relations?: FindOptionsRelations<Organization>)
