e6b9e168a5cea0a2c46cef99a906a95b4790b8e6
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-11-06 20:35:16 +0500

    PermissionsController findAll method is ready

--- src/permissions/permissions.controller.ts
+++ src/permissions/permissions.controller.ts
@@ -1,7 +1,10 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Permission } from 'src/iam/authorization/decorators/permission.decorator';
 import { CreatePermissionDto } from './dto/create-permission.dto';
+import { PermissionPageFilterDto } from './dto/permission-page-filter.dto';
 import { UpdatePermissionDto } from './dto/update-permission.dto';
+import { PermissionPermissions } from './enums/permission-permissions.enum';
 import { PermissionsService } from './permissions.service';
 
 @ApiTags('permissions')
@@ -11,30 +14,35 @@ export class PermissionsController
     constructor (private readonly permissionsService: PermissionsService) { }
 
     @Post()
+    @Permission(PermissionPermissions.Create)
     create(@Body() createPermissionDto: CreatePermissionDto)
     {
         return this.permissionsService.create(createPermissionDto);
     }
 
     @Get()
-    findAll()
+    @Permission(PermissionPermissions.Read)
+    findAll(@Query() pageFilterDto: PermissionPageFilterDto)
     {
-        return this.permissionsService.findAll();
+        return this.permissionsService.findAllWithFilters(pageFilterDto);
     }
 
     @Get(':id')
+    @Permission(PermissionPermissions.Read)
     findOne(@Param('id') id: string)
     {
         return this.permissionsService.findOne({ id: +id });
     }
 
     @Put(':id')
+    @Permission(PermissionPermissions.Update)
     update(@Param('id') id: string, @Body() updatePermissionDto: UpdatePermissionDto)
     {
         return this.permissionsService.update(+id, updatePermissionDto);
     }
 
     @Delete(':id')
+    @Permission(PermissionPermissions.Delete)
     remove(@Param('id') id: string)
     {
         return this.permissionsService.remove(+id);
