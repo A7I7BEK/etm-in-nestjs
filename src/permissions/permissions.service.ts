95eb4736f89c4299ce5a6d7acfa2d1c56a37f0f4
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-11-06 20:34:39 +0500

    PermissionsService findAllWithFilters method is ready

--- src/permissions/permissions.service.ts
+++ src/permissions/permissions.service.ts
@@ -1,7 +1,11 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { FindManyOptions, FindOptionsRelations, FindOptionsWhere, Repository } from 'typeorm';
+import { OrderReverse } from 'src/common/pagination/order.enum';
+import { PaginationMeta } from 'src/common/pagination/pagination-meta.class';
+import { Pagination } from 'src/common/pagination/pagination.class';
+import { Brackets, FindManyOptions, FindOptionsRelations, FindOptionsWhere, Repository } from 'typeorm';
 import { CreatePermissionDto } from './dto/create-permission.dto';
+import { PermissionPageFilterDto } from './dto/permission-page-filter.dto';
 import { UpdatePermissionDto } from './dto/update-permission.dto';
 import { Permission } from './entities/permission.entity';
 
@@ -24,6 +28,31 @@ export class PermissionsService
     findAll(options?: FindManyOptions<Permission>)
     {
         return this.permissionsRepository.find(options);
+    }
+
+    async findAllWithFilters(pageFilterDto: PermissionPageFilterDto)
+    {
+        const queryBuilder = this.permissionsRepository.createQueryBuilder('perm');
+        queryBuilder.skip(pageFilterDto.skip);
+        queryBuilder.take(pageFilterDto.perPage);
+        queryBuilder.orderBy('perm.' + pageFilterDto.sortBy, OrderReverse[ pageFilterDto.sortDirection ]);
+
+        if (pageFilterDto.allSearch)
+        {
+            queryBuilder.andWhere(
+                new Brackets((qb) =>
+                {
+                    qb.orWhere('perm.name ILIKE :search', { search: `%${pageFilterDto.allSearch}%` });
+                    qb.orWhere('perm.codeName ILIKE :search', { search: `%${pageFilterDto.allSearch}%` });
+                }),
+            );
+        }
+
+        const [ data, total ] = await queryBuilder.getManyAndCount();
+
+        const paginationMeta = new PaginationMeta(pageFilterDto.page, pageFilterDto.perPage, total);
+
+        return new Pagination<Permission>(data, paginationMeta);
     }
 
     async findOne(where: FindOptionsWhere<Permission>, relations?: FindOptionsRelations<Permission>)
