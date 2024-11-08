8bbdc733d956bbaebb36e815b1a4c34ce1453703
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-11-08 22:36:35 +0500

    CreateProjectDto is ready

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -1,1 +1,30 @@
-export class CreateProjectDto { }
+import { Type } from 'class-transformer';
+import { IsEnum, IsInt, IsNotEmpty, IsString, Min, ValidateNested } from 'class-validator';
+import { ObjectIdDto } from 'src/common/dto/object-id.dto';
+import { ProjectType } from '../enums/project-type';
+
+export class CreateProjectDto
+{
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsString()
+    @IsNotEmpty()
+    codeName: string;
+
+    @IsEnum(ProjectType)
+    projectType: ProjectType;
+
+    @ValidateNested()
+    @Type(() => ObjectIdDto)
+    group: ObjectIdDto;
+
+    @ValidateNested()
+    @Type(() => ObjectIdDto)
+    manager: ObjectIdDto;
+
+    @IsInt()
+    @Min(0)
+    organizationId: number;
+}
