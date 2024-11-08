c8a2c23af4c356d1d89d7df28ecfcd54aaf75658
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-11-08 21:47:30 +0500

    ProjectProperties enum created

---
+++ src/projects/enums/project-properties.enum.ts
@@ -0,0 +1,11 @@
+export enum ProjectProperties
+{
+    ID = 'id',
+    NAME = 'name',
+    CODENAME = 'codeName',
+    PROJECTTYPE = 'projectType',
+    GROUP = 'group',
+    MANAGER = 'manager',
+    PERCENT = 'percent',
+    ORGANIZATION = 'organization',
+}
