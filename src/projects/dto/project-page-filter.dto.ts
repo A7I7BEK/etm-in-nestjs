2aaef360d4b7380a198bee979e552605c6a8fdf3
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-11-08 21:47:47 +0500

    ProjectPageFilterDto class created

---
+++ src/projects/dto/project-page-filter.dto.ts
@@ -0,0 +1,34 @@
+import { Type } from 'class-transformer';
+import { IsEnum, IsInt, IsOptional, Min } from 'class-validator';
+import { PageFilterDto } from 'src/common/dto/page-filter.dto';
+import { ProjectProperties } from '../enums/project-properties.enum';
+import { ProjectType } from '../enums/project-type';
+
+export class ProjectPageFilterDto extends PageFilterDto<ProjectProperties>
+{
+    @IsOptional()
+    @IsEnum(ProjectProperties)
+    sortBy?: ProjectProperties = ProjectProperties.ID;
+
+    @IsOptional()
+    @Min(1)
+    @IsInt()
+    @Type(() => Number)
+    groupId?: number;
+
+    @IsOptional()
+    @Min(1)
+    @IsInt()
+    @Type(() => Number)
+    managerId?: number;
+
+    @IsOptional()
+    @Min(1)
+    @IsInt()
+    @Type(() => Number)
+    organizationId?: number;
+
+    @IsOptional()
+    @IsEnum(ProjectType)
+    projectType?: ProjectType;
+}
