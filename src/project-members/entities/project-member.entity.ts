a0458826cf7c39e2546eda7ea862b7206e199918
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-11-09 18:48:33 +0500

    ProjectMember entity is ready

--- src/project-members/entities/project-member.entity.ts
+++ src/project-members/entities/project-member.entity.ts
@@ -1,1 +1,25 @@
-export class ProjectMember {}
+import { Employee } from 'src/employees/entities/employee.entity';
+import { Project } from 'src/projects/entities/project.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class ProjectMember
+{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    isOnline: boolean = false;
+
+    @Column()
+    lastSeenTime: Date = new Date();
+
+    @ManyToOne(type => Employee)
+    employee: Employee;
+
+    @Column()
+    isTeamLeader: boolean;
+
+    @ManyToOne(type => Project, p => p.members)
+    project: Project;
+}
