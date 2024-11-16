90154b7990a51b81474c56ea4886794498df21ef
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-11-16 21:26:36 +0500

    UserCreateDto refactoring

--- src/users/dto/user-create.dto.ts
+++ src/users/dto/user-create.dto.ts
@@ -1,26 +1,21 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsInt, IsNotEmpty, IsNumberString, IsPhoneNumber, IsString, Min, MinLength } from 'class-validator';
 
 export class UserCreateDto
 {
-    @IsInt()
     @Min(0)
+    @IsInt()
     organizationId: number;
 
-    @ApiProperty({ example: 'johnny' })
     @IsNotEmpty()
     @IsString()
     userName: string;
 
-    @ApiProperty({ example: 'pass:123456789' })
     @MinLength(10)
     password: string;
 
-    @ApiProperty({ example: 'aaa@aaa.aaa' })
     @IsEmail()
     email: string;
 
-    @ApiProperty({ example: '901234567' })
     @IsPhoneNumber('UZ')
     @IsNumberString({ no_symbols: true })
     phoneNumber: string;
