f4d65e46c06d959239b36d7a6214d66a403d9a6d
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-12-01 18:27:54 +0500

    TaskProperties is ready

--- src/tasks/enums/task-properties.enum.ts
+++ src/tasks/enums/task-properties.enum.ts
@@ -1,7 +1,23 @@
+
+/**
+ * temporary for this project:
+ * properties are not appropriate,
+ * change them to the real ones
+ */
 export enum TaskProperties
 {
     ID = 'id',
-    ROLE_NAME = 'roleName',
-    CODE_NAME = 'codeName',
-    ORGANIZATION = 'organization',
+    NAME = 'name',
+    DEADLINE = 'deadLine',
+    COLUMN_NAME = 'columnName',
+    PROJECT_NAME = 'projectName',
 }
+
+
+export const TaskPropertiesReal = {
+    [ TaskProperties.ID ]: 'id',
+    [ TaskProperties.NAME ]: 'name',
+    [ TaskProperties.DEADLINE ]: 'endDate',
+    [ TaskProperties.COLUMN_NAME ]: 'column',
+    [ TaskProperties.PROJECT_NAME ]: 'project',
+} as const;
