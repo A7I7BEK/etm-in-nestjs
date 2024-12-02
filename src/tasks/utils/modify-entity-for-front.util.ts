ff47818b1268eb2bf7d14c9073dcac6685b4d39b
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-12-03 00:05:48 +0500

    modifyTaskForFront is ready

--- src/tasks/utils/modify-entity-for-front.util.ts
+++ src/tasks/utils/modify-entity-for-front.util.ts
@@ -1,18 +1,45 @@
 import { Task } from '../entities/task.entity';
+import { TaskLevel } from '../enums/task-level.enum';
+import { TaskPriority } from '../enums/task-priority.enum';
 
 /**
  * temporary for this project, must not exist
  */
-export function modifyEntityForFront(entity: Task)
+export function modifyTaskForFront(entity: Task)
 {
-    const { organization } = entity;
+    const { project, column } = entity;
 
 
-    if (organization)
+    Object.assign(entity, {
+        startAt: entity.createdAt,
+        deadLine: entity.endDate,
+        taskLevelType: {
+            id: entity.level,
+            name: TaskLevel[ entity.level ],
+            value: TaskLevel[ entity.level ],
+        },
+        taskPriorityType: {
+            id: entity.priority,
+            name: TaskPriority[ entity.priority ],
+            value: TaskPriority[ entity.priority ],
+        },
+    });
+
+
+    if (project)
     {
         Object.assign(entity, {
-            organizationId: organization.id,
-            organizationName: organization.name,
+            projectId: project.id,
+            projectName: project.name,
+        });
+    }
+
+
+    if (column)
+    {
+        Object.assign(entity, {
+            columnId: column.id,
+            columnName: column.name,
         });
     }
 
