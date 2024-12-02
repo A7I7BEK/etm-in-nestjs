29a1395d0066449cfd7e836e351d4436ba02266e
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-12-02 23:58:11 +0500

    task createEntity is ready

---
+++ src/tasks/utils/create-entity.util.ts
@@ -0,0 +1,41 @@
+import { ActiveUserData } from 'src/iam/interfaces/active-user-data.interface';
+import { ProjectColumnsService } from 'src/project-columns/project-columns.service';
+import { Repository } from 'typeorm';
+import { TaskCreateDto } from '../dto/task-create.dto';
+import { Task } from '../entities/task.entity';
+
+
+export async function createEntity
+    (
+        columnsService: ProjectColumnsService,
+        repository: Repository<Task>,
+        dto: TaskCreateDto,
+        activeUser: ActiveUserData,
+        entity = new Task(),
+    )
+{
+    const columnEntity = await columnsService.findOne(
+        {
+            where: {
+                id: dto.columnId,
+                project: {
+                    id: dto.projectId,
+                }
+            },
+            relations: {
+                project: true,
+                tasks: true,
+            }
+        },
+        activeUser,
+    );
+
+
+    entity.name = dto.name;
+    entity.column = columnEntity;
+    entity.project = columnEntity.project;
+    entity.ordering = columnEntity.tasks.length;
+
+
+    return repository.save(entity);
+}
