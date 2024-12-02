a45f256b56fdad1ef2f584cccc4d1e05d76c4e55
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-12-02 22:42:31 +0500

    TaskUpdateDto is ready

--- src/tasks/dto/task-update.dto.ts
+++ src/tasks/dto/task-update.dto.ts
@@ -1,3 +1,23 @@
-import { TaskCreateDto } from './task-create.dto';
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { TaskLevel } from '../enums/task-level.enum';
+import { TaskPriority } from '../enums/task-priority.enum';
 
-export class TaskUpdateDto extends TaskCreateDto { }
+export class TaskUpdateDto
+{
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+
+    @IsOptional()
+    @IsNotEmpty()
+    @IsString()
+    description?: string;
+
+    @IsOptional()
+    @IsEnum(TaskLevel)
+    taskLevelType?: TaskLevel;
+
+    @IsOptional()
+    @IsEnum(TaskPriority)
+    taskPriorityType?: TaskPriority;
+}
