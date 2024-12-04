18c97a49a83d26194692ee7ee10d4ac4d20b52cb
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-12-05 00:53:27 +0500

    TaskMembersModule is ready

--- src/task-members/task-members.module.ts
+++ src/task-members/task-members.module.ts
@@ -1,7 +1,7 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { OrganizationsModule } from 'src/organizations/organizations.module';
-import { PermissionsModule } from 'src/permissions/permissions.module';
+import { EmployeesModule } from 'src/employees/employees.module';
+import { TasksModule } from 'src/tasks/tasks.module';
 import { TaskMember } from './entities/task-member.entity';
 import { TaskMembersController } from './task-members.controller';
 import { TaskMembersService } from './task-members.service';
@@ -9,8 +9,8 @@ import { TaskMembersService } from './task-members.service';
 @Module({
     imports: [
         TypeOrmModule.forFeature([ TaskMember ]),
-        OrganizationsModule,
-        PermissionsModule,
+        forwardRef(() => TasksModule),
+        EmployeesModule,
     ],
     exports: [ TaskMembersService ],
     controllers: [ TaskMembersController ],
