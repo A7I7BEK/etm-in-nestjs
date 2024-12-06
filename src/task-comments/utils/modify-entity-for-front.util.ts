c9c26adb26a41507cf08599567bfb49d26c790b4
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-12-06 15:07:45 +0500

    task-comments modifyEntityForFront is ready

--- src/task-comments/utils/modify-entity-for-front.util.ts
+++ src/task-comments/utils/modify-entity-for-front.util.ts
@@ -5,14 +5,46 @@ import { TaskComment } from '../entities/task-comment.entity';
  */
 export function modifyEntityForFront(entity: TaskComment)
 {
-    const { organization } = entity;
+    const { author, members, task } = entity;
 
 
-    if (organization)
+    if (author)
     {
         Object.assign(entity, {
-            organizationId: organization.id,
-            organizationName: organization.name,
+            authorId: author.id,
+            firstName: author.firstName,
+            lastName: author.lastName,
+            middleName: author.middleName,
+            photoUrl: author.photoUrl,
+            birthDate: author.birthDate,
+            userId: author.user?.id,
+            userName: author.user?.userName,
+        });
+    }
+
+
+    if (members)
+    {
+        members.forEach(item =>
+        {
+            Object.assign(item, {
+                employee: {
+                    firstName: item.firstName,
+                    lastName: item.lastName,
+                    middleName: item.middleName,
+                    userId: item.user?.id,
+                    userName: item.user?.userName,
+                }
+            });
+        });
+    }
+
+
+    if (task)
+    {
+        Object.assign(entity, {
+            taskId: task.id,
+            taskName: task.name,
         });
     }
 
