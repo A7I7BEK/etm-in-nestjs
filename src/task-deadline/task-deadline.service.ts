7d2d0edcf4a9ccabcce8c258ae377610e9152e92
Azizbek Iskandarov <a7i7bek@gmail.com>
2024-12-08 16:20:34 +0500

    TaskDeadlineService is ready

--- src/task-deadline/task-deadline.service.ts
+++ src/task-deadline/task-deadline.service.ts
@@ -7,6 +7,7 @@ import { TaskDeadlineCreateDto } from './dto/task-deadline-create.dto';
 import { TaskDeadlineDeleteDto } from './dto/task-deadline-delete.dto';
 import { TaskDeadlineUpdateDto } from './dto/task-deadline-update.dto';
 import { TaskDeadline } from './entities/task-deadline.entity';
+import { TaskDeadlineAction } from './enums/task-deadline-action.enum';
 
 @Injectable()
 export class TaskDeadlineService
@@ -18,13 +19,32 @@ export class TaskDeadlineService
     ) { }
 
 
-    create
+    async create
         (
             createDto: TaskDeadlineCreateDto,
             activeUser: ActiveUserData,
         )
     {
+        const entity = new TaskDeadline();
 
+        entity.task = await this._tasksService.findOne(
+            {
+                where: {
+                    id: createDto.taskId
+                }
+            },
+            activeUser
+        );
+
+        entity.action = TaskDeadlineAction.Create;
+        entity.details = createDto;
+        entity.comment = TaskDeadlineAction.Create;
+        entity.createdAt = new Date();
+        await this.repository.save(entity);
+
+        entity.task.startDate = new Date(createDto.startDate);
+        entity.task.endDate = new Date(createDto.deadLine);
+        return this._tasksService.repository.save(entity.task);
     }
 
 
@@ -34,7 +54,26 @@ export class TaskDeadlineService
             activeUser: ActiveUserData,
         )
     {
+        const entity = new TaskDeadline();
 
+        entity.task = await this._tasksService.findOne(
+            {
+                where: {
+                    id: updateDto.taskId
+                }
+            },
+            activeUser
+        );
+
+        entity.action = TaskDeadlineAction.Update;
+        entity.details = updateDto;
+        entity.comment = updateDto.changesComment;
+        entity.createdAt = new Date();
+        await this.repository.save(entity);
+
+        entity.task.startDate = new Date(updateDto.startDate);
+        entity.task.endDate = new Date(updateDto.deadLine);
+        return this._tasksService.repository.save(entity.task);
     }
 
 
@@ -44,6 +83,33 @@ export class TaskDeadlineService
             activeUser: ActiveUserData,
         )
     {
+        const entity = new TaskDeadline();
 
+        entity.task = await this._tasksService.findOne(
+            {
+                where: {
+                    id: deleteDto.taskId
+                }
+            },
+            activeUser
+        );
+
+        entity.action = TaskDeadlineAction.Delete;
+        entity.details = deleteDto;
+        entity.comment = deleteDto.changesComment;
+        entity.createdAt = new Date();
+        await this.repository.save(entity);
+
+        if (deleteDto.startDate)
+        {
+            entity.task.startDate = null;
+        }
+
+        if (deleteDto.deadLine)
+        {
+            entity.task.endDate = null;
+        }
+
+        return this._tasksService.repository.save(entity.task);
     }
 }
