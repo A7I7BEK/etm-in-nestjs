5ee5e336b0eb97bbbdcbfc2ba572add9a7c53bc5
Azizbek Iskandar <a7i7bek@gmail.com>
2024-12-09 22:48:15 +0500

    CheckListItemsModule is done

--- src/check-list-items/check-list-items.module.ts
+++ src/check-list-items/check-list-items.module.ts
@@ -1,16 +1,18 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { OrganizationsModule } from 'src/organizations/organizations.module';
-import { PermissionsModule } from 'src/permissions/permissions.module';
-import { CheckListItem } from './entities/check-list-item.entity';
+import { CheckListGroupsModule } from 'src/check-list-groups/check-list-groups.module';
+import { EmployeesModule } from 'src/employees/employees.module';
+import { TasksModule } from 'src/tasks/tasks.module';
 import { CheckListItemsController } from './check-list-items.controller';
 import { CheckListItemsService } from './check-list-items.service';
+import { CheckListItem } from './entities/check-list-item.entity';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([ CheckListItem ]),
-        OrganizationsModule,
-        PermissionsModule,
+        forwardRef(() => CheckListGroupsModule),
+        TasksModule,
+        EmployeesModule,
     ],
     exports: [ CheckListItemsService ],
     controllers: [ CheckListItemsController ],
