b9fdf59bd4f899a5b1132145fbd74e45dd9a3aee
Azizbek Iskandar <a7i7bek@gmail.com>
2024-12-09 19:32:50 +0500

    CheckListItemUpdateDto is ready

--- src/check-list-items/dto/check-list-item-update.dto.ts
+++ src/check-list-items/dto/check-list-item-update.dto.ts
@@ -1,3 +1,10 @@
+import { OmitType } from '@nestjs/swagger';
+import { IsBoolean } from 'class-validator';
 import { CheckListItemCreateDto } from './check-list-item-create.dto';
 
-export class CheckListItemUpdateDto extends CheckListItemCreateDto { }
+export class CheckListItemUpdateDto extends
+    OmitType(CheckListItemCreateDto, [ 'members', 'checkListGroupId' ])
+{
+    @IsBoolean()
+    checked: boolean;
+}
