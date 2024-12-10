a2a804cb4ccd9accd65ffa6ca12b9332df8baf53
Azizbek Iskandar <a7i7bek@gmail.com>
2024-12-10 11:45:14 +0500

    check-list-groups create-update-entity.util.ts is ready

--- src/check-list-groups/utils/create-update-entity.util.ts
+++ src/check-list-groups/utils/create-update-entity.util.ts
@@ -1,7 +1,5 @@
 import { ActiveUserData } from 'src/iam/interfaces/active-user-data.interface';
-import { OrganizationsService } from 'src/organizations/organizations.service';
-import { PermissionsService } from 'src/permissions/permissions.service';
-import { In, Repository } from 'typeorm';
+import { CheckListGroupsService } from '../check-list-groups.service';
 import { CheckListGroupCreateDto } from '../dto/check-list-group-create.dto';
 import { CheckListGroupUpdateDto } from '../dto/check-list-group-update.dto';
 import { CheckListGroup } from '../entities/check-list-group.entity';
@@ -9,31 +7,25 @@ import { CheckListGroup } from '../entities/check-list-group.entity';
 
 export async function createUpdateEntity
     (
-        organizationsService: OrganizationsService,
-        permissionsService: PermissionsService,
-        repository: Repository<CheckListGroup>,
+        service: CheckListGroupsService,
         dto: CheckListGroupCreateDto | CheckListGroupUpdateDto,
         activeUser: ActiveUserData,
         entity = new CheckListGroup(),
     )
 {
-    const organizationEntity = await organizationsService.findOneActiveUser(
-        {
-            where: { id: dto.organizationId }
-        },
-        activeUser,
-    );
+    if (dto instanceof CheckListGroupCreateDto)
+    {
+        entity.task = await service.tasksService.findOne(
+            {
+                where: { id: dto.taskId }
+            },
+            activeUser,
+        );
+    }
 
 
-    const permissionIds = dto.permissions.map(x => x.id); // temporary for this project, must be: [1, 2, 3]
-    const permissionEntities = await permissionsService.findAll({ where: { id: In(permissionIds) } }); // BINGO
+    entity.name = dto.name;
 
 
-    entity.roleName = dto.roleName;
-    entity.codeName = dto.codeName;
-    entity.organization = organizationEntity;
-    entity.permissions = permissionEntities;
-
-
-    return repository.save(entity);
+    return service.repository.save(entity);
 }
