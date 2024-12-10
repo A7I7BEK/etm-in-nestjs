225c717d0c4492de692849cbfe32a973cca8d512
Azizbek Iskandar <a7i7bek@gmail.com>
2024-12-10 11:52:25 +0500

    CheckListItemsModule TasksModule removed

--- src/check-list-items/check-list-items.module.ts
+++ src/check-list-items/check-list-items.module.ts
@@ -1,8 +1,7 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CheckListGroupsModule } from 'src/check-list-groups/check-list-groups.module';
 import { EmployeesModule } from 'src/employees/employees.module';
-import { TasksModule } from 'src/tasks/tasks.module';
 import { CheckListItemsController } from './check-list-items.controller';
 import { CheckListItemsService } from './check-list-items.service';
 import { CheckListItem } from './entities/check-list-item.entity';
@@ -10,8 +9,7 @@ import { CheckListItem } from './entities/check-list-item.entity';
 @Module({
     imports: [
         TypeOrmModule.forFeature([ CheckListItem ]),
-        forwardRef(() => CheckListGroupsModule),
-        TasksModule,
+        CheckListGroupsModule,
         EmployeesModule,
     ],
     exports: [ CheckListItemsService ],
