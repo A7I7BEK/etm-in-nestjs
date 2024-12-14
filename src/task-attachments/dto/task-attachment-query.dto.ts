43f298002667b90d2c3c88306a9b6b21764489e2
Azizbek Iskandar <a7i7bek@gmail.com>
2024-12-14 14:16:20 +0500

    TaskAttachmentQueryDto refactored

--- src/task-attachments/dto/task-attachment-query.dto.ts
+++ src/task-attachments/dto/task-attachment-query.dto.ts
@@ -1,8 +1,14 @@
 import { Type } from 'class-transformer';
-import { IsInt, Min } from 'class-validator';
+import { IsEnum, IsInt, IsOptional, Min } from 'class-validator';
+import { BaseQueryDto } from 'src/common/dto/base-query.dto';
+import { TaskAttachmentProperties } from '../enums/task-attachment-properties.enum';
 
-export class TaskAttachmentQueryDto
+export class TaskAttachmentQueryDto extends BaseQueryDto<TaskAttachmentProperties>
 {
+    @IsOptional()
+    @IsEnum(TaskAttachmentProperties)
+    sortBy?: TaskAttachmentProperties = TaskAttachmentProperties.ID;
+
     @Min(1)
     @IsInt()
     @Type(() => Number)
