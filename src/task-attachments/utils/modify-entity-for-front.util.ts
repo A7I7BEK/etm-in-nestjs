a6e63d12b12bc8016f40c5d4fd04e260fea87132
Azizbek Iskandar <a7i7bek@gmail.com>
2024-12-14 14:19:09 +0500

    task-attachments modifyEntityForFront created

---
+++ src/task-attachments/utils/modify-entity-for-front.util.ts
@@ -0,0 +1,22 @@
+import { TaskAttachment } from '../entities/task-attachment.entity';
+
+/**
+ * temporary for this project, must not exist
+ */
+export function modifyEntityForFront(entity: TaskAttachment)
+{
+    const { file } = entity;
+
+
+    if (file)
+    {
+        const { id, ...rest } = file;
+        Object.assign(entity, {
+            fileId: id,
+            ...rest
+        });
+    }
+
+
+    return entity;
+}
