d98609277c0025b415e98c5a2e8b05db9e696076
Azizbek Iskandar <a7i7bek@gmail.com>
2024-12-14 14:15:28 +0500

    TaskAttachment entity refactored

--- src/task-attachments/entities/task-attachment.entity.ts
+++ src/task-attachments/entities/task-attachment.entity.ts
@@ -1,10 +1,16 @@
 import { Resource } from 'src/resource/entities/resource.entity';
 import { Task } from 'src/tasks/entities/task.entity';
-import { Entity, ManyToOne } from 'typeorm';
+import { Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class TaskAttachment extends Resource // TODO: check experimental
+export class TaskAttachment
 {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @ManyToOne(type => Resource, { eager: true })
+    file: Resource;
+
     @ManyToOne(type => Task, t => t.attachments)
     task: Task;
 }
