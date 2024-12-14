fdd3655186377a573567c5d3e46d2e86ea6500da
Azizbek Iskandar <a7i7bek@gmail.com>
2024-12-14 14:16:35 +0500

    TaskAttachmentPermissions fix

--- src/task-attachments/enums/task-attachment-permissions.enum.ts
+++ src/task-attachments/enums/task-attachment-permissions.enum.ts
@@ -1,6 +1,6 @@
 export enum TaskAttachmentPermissions
 {
-    Create = 'TASK_ATTACHMENT_BIND',
+    Create = 'TASK_ATTACHMENT_CREATE',
     Read = 'TASK_ATTACHMENT_READ',
     Delete = 'TASK_ATTACHMENT_DELETE',
 }
