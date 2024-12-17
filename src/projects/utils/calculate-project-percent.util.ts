e7ac04899557db6d29c65413f8f1d1482e172db3
Azizbek Iskandar <a7i7bek@gmail.com>
2024-12-17 16:31:10 +0500

    calculateProjectPercent created

---
+++ src/projects/utils/calculate-project-percent.util.ts
@@ -0,0 +1,22 @@
+import { ProjectColumnTypeKanban } from 'src/project-columns/enums/project-column-type-kanban.enum';
+import { Project } from '../entities/project.entity';
+import { ProjectType } from '../enums/project-type.enum';
+
+
+export function calculateProjectPercent(entity: Project)
+{
+    const { tasks } = entity;
+
+
+    if (entity.projectType === ProjectType.KANBAN && tasks)
+    {
+        const totalCount = tasks.length;
+        const checkedCount = tasks.filter(item => item.column?.codeName === ProjectColumnTypeKanban.ARCHIVE).length;
+        const percent = Math.floor(checkedCount / totalCount * 100);
+
+        Object.assign(entity, { percent });
+    }
+
+
+    return entity;
+}
