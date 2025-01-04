9f5a576eb2dad0752627041366b75d16bdc79978
Azizbek Iskandar <a7i7bek@gmail.com>
2025-01-04 14:00:09 +0500

    SocketGateway authentication is integrated

--- src/socket/socket.gateway.ts
+++ src/socket/socket.gateway.ts
@@ -1,12 +1,22 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import appConfig from 'src/common/config/app.config';
 
-@WebSocketGateway()
+@WebSocketGateway({
+    path: '/web-socket',
+    cors: { origin: '*' },
+})
 @Injectable()
 export class SocketGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
-    private logger: Logger = new Logger('AppSocketGateway');
+    constructor (
+        private readonly jwtService: JwtService,
+    ) { }
+
+
+    private logger: Logger = new Logger('SocketGateway');
 
     @WebSocketServer()
     server: Server;
@@ -14,18 +24,38 @@ export class SocketGateway implements OnGatewayInit, OnGatewayConnection, OnGate
 
     afterInit(server: Server)
     {
-        this.logger.log(`Server Initialized: ${server.engine.clientsCount}`);
+        this.logger.log(`WebSocket server Initialized: ${server.engine.clientsCount}`);
+        console.log('WebSocket server Initialized: ', server);
     }
 
-    handleConnection(client: Socket, ...args: any[])
+    async handleConnection(client: Socket, ...args: any[])
     {
-        this.logger.log(`Client Connected: ${client.id}`);
-        this.logger.log(`Client Args: ${args}`);
+        console.log(`WebSocket client Connected Args: ${args}`);
+
+        try
+        {
+            const token = client.handshake.auth.token as string;
+            const payload = await this.jwtService.verifyAsync(
+                token,
+                {
+                    secret: appConfig().jwt.accessTokenSecret,
+                    audience: appConfig().jwt.audience,
+                    issuer: appConfig().jwt.issuer,
+                }
+            );
+
+            this.logger.log(`WebSocket client Connected: ${client.id} (User ID: ${payload.sub})`);
+        }
+        catch (error)
+        {
+            this.logger.error('WebSocket invalid token, disconnecting...');
+            client.disconnect(true);
+        }
     }
 
     handleDisconnect(client: Socket)
     {
-        this.logger.log(`Client Disconnected: ${client.id}`);
+        this.logger.log(`WebSocket client Disconnected: ${client.id}`);
     }
 
 
