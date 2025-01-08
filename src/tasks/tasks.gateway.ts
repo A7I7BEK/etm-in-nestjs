2c13c708a7f3b77118de65727bea6a6f0eeed985
Azizbek Iskandar <a7i7bek@gmail.com>
2025-01-08 15:19:46 +0500

    TasksGateway created end embedded

---
+++ src/tasks/tasks.gateway.ts
@@ -0,0 +1,66 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { Task } from './entities/task.entity';
+
+@WebSocketGateway({
+    path: '/ws-tasks',
+    cors: { origin: '*' },
+})
+@Injectable()
+export class TasksGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+    private logger: Logger = new Logger('TasksGateway');
+
+    @WebSocketServer()
+    server: Server;
+
+    roomName: 'project-';
+    roomId: string;
+
+
+    afterInit(server: Server)
+    {
+        this.logger.log(`Server Initialized in: ${server._opts.path}`);
+    }
+
+    handleConnection(client: Socket)
+    {
+        this.roomId = client.handshake.query.roomId as string;
+        client.join(this.roomName + this.roomId);
+
+        this.logger.log(`Client Connected: ${client.id}`);
+        console.log('Connected: ', client.handshake);
+    }
+
+    handleDisconnect(client: Socket)
+    {
+        this.logger.log(`Client Disconnected: ${client.id}`);
+    }
+
+
+    emitInsert(payload: Task, roomId: string = this.roomId)
+    {
+        this.server.to(this.roomName + roomId).emit('task-insert', payload);
+    }
+
+    emitDelete(payload: Task, roomId: string = this.roomId)
+    {
+        this.server.to(this.roomName + roomId).emit('task-delete', payload);
+    }
+
+    emitReplace(payload: Task, roomId: string = this.roomId)
+    {
+        this.server.to(this.roomName + roomId).emit('task-replace', payload);
+    }
+
+    emitReorder(payload: Task, roomId: string = this.roomId)
+    {
+        this.server.to(this.roomName + roomId).emit('task-reorder', payload);
+    }
+
+    emitMove(payload: Task, roomId: string = this.roomId)
+    {
+        this.server.to(this.roomName + roomId).emit('task-move', payload);
+    }
+}
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,19 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProjectColumnsModule } from 'src/project-columns/project-columns.module';
-import { SocketModule } from 'src/socket/socket.module';
 import { Task } from './entities/task.entity';
 import { TasksController } from './tasks.controller';
+import { TasksGateway } from './tasks.gateway';
 import { TasksService } from './tasks.service';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([ Task ]),
         ProjectColumnsModule,
-        SocketModule,
     ],
     exports: [ TasksService ],
     controllers: [ TasksController ],
-    providers: [ TasksService ],
+    providers: [
+        TasksService,
+        TasksGateway,
+    ],
 })
 export class TasksModule { }
