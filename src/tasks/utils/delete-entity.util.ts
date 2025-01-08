f6c59b5857aff7690247695b19385b9929d13daa
Azizbek Iskandar <a7i7bek@gmail.com>
2025-01-08 15:23:00 +0500

    task deleteEntity created

---
+++ src/tasks/utils/delete-entity.util.ts
@@ -0,0 +1,48 @@
+import { reOrderItems } from 'src/common/utils/re-order-items.util';
+import { ActiveUserData } from 'src/iam/interfaces/active-user-data.interface';
+import { TasksService } from '../tasks.service';
+
+
+export async function deleteEntity
+    (
+        service: TasksService,
+        id: number,
+        activeUser: ActiveUserData,
+    )
+{
+    const entity = await service.findOne(
+        {
+            where: { id },
+            relations: { column: true }
+        },
+        activeUser,
+    );
+
+
+    await service.repository.remove(entity);
+
+
+    const columnEntity = await service.columnsService.findOne(
+        {
+            where: { id: entity.column.id },
+            relations: { tasks: true },
+            order: {
+                tasks: {
+                    ordering: 'ASC',
+                }
+            }
+        },
+        activeUser,
+    );
+
+
+    reOrderItems(columnEntity.tasks);
+    await service.repository.save(columnEntity.tasks);
+
+
+    entity.id = id;
+    service.tasksGateway.emitDelete(entity);
+
+
+    return entity;
+}
