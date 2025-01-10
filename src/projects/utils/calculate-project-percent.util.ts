f27946f88719f4ac26afc4f4f7fcba2135a9b850
Azizbek Iskandar <a7i7bek@gmail.com>
2025-01-10 23:35:39 +0500

    fix: update calculateProjectPercent to handle empty task arrays and non-KANBAN projects

--- src/projects/utils/calculate-project-percent.util.ts
+++ src/projects/utils/calculate-project-percent.util.ts
@@ -8,15 +8,24 @@ export function calculateProjectPercent(entity: Project)
     const { tasks } = entity;
 
 
-    if (entity.projectType === ProjectType.KANBAN && tasks)
+    if (entity.projectType !== ProjectType.KANBAN || !tasks)
     {
-        const totalCount = tasks.length;
-        const checkedCount = tasks.filter(item => item.column?.codeName === ProjectColumnTypeKanban.ARCHIVE).length;
-        const percent = Math.floor(checkedCount / totalCount * 100);
-
-        Object.assign(entity, { percent });
+        return entity;
     }
 
 
+    if (tasks.length === 0)
+    {
+        Object.assign(entity, { percent: 0 });
+        return entity;
+    }
+
+
+    const totalCount = tasks.length;
+    const checkedCount = tasks.filter(item => item.column.codeName === ProjectColumnTypeKanban.ARCHIVE).length;
+    const percent = Math.floor(checkedCount / totalCount * 100);
+
+
+    Object.assign(entity, { percent });
     return entity;
 }
