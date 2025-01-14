f950fab5fce34affa3ca92087c77fd26cea9a704
Azizbek Iskandar <a7i7bek@gmail.com>
2025-01-14 23:58:16 +0500

    refactor: simplify JWT verification by removing redundant configuration parameters

--- src/iam/authentication/guards/access-token.guard.ts
+++ src/iam/authentication/guards/access-token.guard.ts
@@ -1,7 +1,6 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
-import appConfig from 'src/common/config/app.config';
 import { REQUEST_USER_KEY } from 'src/iam/iam.constants';
 
 @Injectable()
@@ -23,14 +22,7 @@ export class AccessTokenGuard implements CanActivate
 
         try
         {
-            const payload = await this.jwtService.verifyAsync(
-                token,
-                {
-                    secret: appConfig().jwt.accessTokenSecret,
-                    audience: appConfig().jwt.audience,
-                    issuer: appConfig().jwt.issuer,
-                }
-            );
+            const payload = await this.jwtService.verifyAsync(token);
             request[ REQUEST_USER_KEY ] = payload; // bind current user to request (express)
         }
         catch (error)
