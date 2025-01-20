bcab74f8d554fa49eaed67ab4d0825696b35ffc8
Azizbek Iskandar <a7i7bek@gmail.com>
2025-01-20 17:12:18 +0500

    feat: integrate EmployeesModule and TasksModule into ActionsModule and add TaskListener provider

--- src/actions/actions.module.ts
+++ src/actions/actions.module.ts
@@ -1,15 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { EmployeesModule } from 'src/employees/employees.module';
+import { TasksModule } from 'src/tasks/tasks.module';
 import { ActionsController } from './actions.controller';
 import { ActionsService } from './actions.service';
 import { Action } from './entities/action.entity';
+import { TaskListener } from './listener/task.listener';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([ Action ]),
+        EmployeesModule,
+        TasksModule,
     ],
     exports: [ ActionsService ],
     controllers: [ ActionsController ],
-    providers: [ ActionsService ],
+    providers: [
+        ActionsService,
+        TaskListener,
+    ],
 })
 export class ActionsModule { }
