6bdb4bef13294f8090973a331360be2c9b5a6347
Azizbek Iskandar <a7i7bek@gmail.com>
2025-03-12 17:19:48 +0500

    feat: enhance MailService to support task sharing and update email templates

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,28 +1,29 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { HttpException, Injectable } from '@nestjs/common';
 import appConfig from 'src/common/config/app.config';
-import { User } from 'src/users/entities/user.entity';
+import { Employee } from 'src/employees/entities/employee.entity';
+import { Task } from 'src/tasks/entities/task.entity';
 import { MailThrottleService } from './mail-throttle.service';
 
 @Injectable()
 export class MailService
 {
     constructor (
-        private readonly mailerService: MailerService,
-        private readonly mailThrottleService: MailThrottleService,
+        private readonly _mailerService: MailerService,
+        private readonly _mailThrottleService: MailThrottleService,
     ) { }
 
 
-    async sendOtpCodeEmail(email: string, code: string)
+    async sendOtpCodeEmail(userId: number, email: string, code: string)
     {
-        await this.mailThrottleService.checkThrottle(email);
+        await this._mailThrottleService.checkThrottle(userId);
 
         try
         {
-            await this.mailerService.sendMail({
+            await this._mailerService.sendMail({
                 to: email,
                 subject: `${code} is your ${appConfig().application.name} verification code`,
-                template: './confirmation-email',
+                template: './confirm-email-simple',
                 context: {
                     code,
                 },
@@ -33,22 +34,26 @@ export class MailService
             throw new HttpException(error.response, error.responseCode);
         }
 
-        await this.mailThrottleService.setThrottle(email);
+        await this._mailThrottleService.setThrottle(userId);
     }
 
 
-    async sendOtpCodeUser(user: User, code: string)
+    async sendOtpCodeUser(sender: Employee, code: string)
     {
-        await this.mailThrottleService.checkThrottle(user.email);
+        await this._mailThrottleService.checkThrottle(sender.user.id);
 
         try
         {
-            await this.mailerService.sendMail({
-                to: user.email,
+            await this._mailerService.sendMail({
+                to: {
+                    name: `${sender.firstName} ${sender.lastName}`,
+                    address: sender.user.email,
+                },
                 subject: `${code} is your ${appConfig().application.name} verification code`,
-                template: './confirmation-user',
+                template: './confirm-email-user',
                 context: {
-                    name: user.userName,
+                    firstName: sender.firstName,
+                    lastName: sender.lastName,
                     code,
                 },
             });
@@ -58,6 +63,39 @@ export class MailService
             throw new HttpException(error.response, error.responseCode);
         }
 
-        await this.mailThrottleService.setThrottle(user.email);
+        await this._mailThrottleService.setThrottle(sender.user.id);
+    }
+
+
+    async shareTask(sender: Employee, task: Task, receiverList: Employee[])
+    {
+        await this._mailThrottleService.checkThrottle(sender.user.id);
+
+        const receiverAddressList = receiverList.map(employee => ({
+            name: `${employee.firstName} ${employee.lastName}`,
+            address: employee.user.email,
+        }));
+
+        try
+        {
+            await this._mailerService.sendMail({
+                to: receiverAddressList,
+                subject: `${sender.firstName} ${sender.lastName} has shared a task with you`,
+                template: './share-task',
+                context: {
+                    firstName: sender.firstName,
+                    lastName: sender.lastName,
+                    taskName: task.name,
+                    taskDescription: task.description,
+                    taskUrl: appConfig().application.urlFront + '?taskShare=' + task.id,
+                },
+            });
+        }
+        catch (error)
+        {
+            throw new HttpException(error.response, error.responseCode);
+        }
+
+        await this._mailThrottleService.setThrottle(sender.user.id);
     }
 }
