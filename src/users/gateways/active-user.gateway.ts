a0af339c4fd0d8e5925146ab9022ecd3b523632b
Azizbek Iskandar <a7i7bek@gmail.com>
2025-03-22 22:45:25 +0500

    temp: update ActiveUsersGateway to track online status and last online time for users

--- src/users/gateways/active-user.gateway.ts
+++ src/users/gateways/active-user.gateway.ts
@@ -2,9 +2,8 @@ import { Injectable, Logger } from '@nestjs/common';
 import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import appConfig from 'src/common/config/app.config';
+import { AccessTokenData } from 'src/iam/jwt/interfaces/access-token-data.interface';
 import { JwtCustomService } from 'src/iam/jwt/jwt-custom.service';
-import { RedisStorageService } from 'src/redis-storage/redis-storage.service';
-import { Equal, In } from 'typeorm';
 import { UsersService } from '../users.service';
 import { WS_ACTIVE_USER_EMIT, WS_ACTIVE_USER_PATH } from './active-user-gateway.constant';
 
@@ -22,42 +21,23 @@ import { WS_ACTIVE_USER_EMIT, WS_ACTIVE_USER_PATH } from './active-user-gateway.
 @Injectable()
 export class ActiveUsersGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
-    constructor (
-        private readonly _service: UsersService,
-        private readonly _jwtService: JwtCustomService,
-        private readonly _redisStorage: RedisStorageService,
-    ) { }
-
-
     @WebSocketServer()
     private readonly server: Server;
     private readonly roomPrefix = 'organization-';
     private readonly logger = new Logger(ActiveUsersGateway.name);
 
 
-    private keyName(orgId: number): string
+    constructor (
+        private readonly _service: UsersService,
+        private readonly _jwtService: JwtCustomService,
+    ) { }
+
+
+    private emitActiveUser(payload: any, roomId: string | number)
     {
-        return `ACTIVE_USERS:${this.roomPrefix}${orgId}`;
-    }
-
-
-    private async emitActiveUsers(orgId: number)
-    {
-        const activeUsers = await this._redisStorage.redisClient
-            .smembers(this.keyName(orgId));
-
-        const userEntityList = await this._service.repository.find({
-            where: {
-                id: In(activeUsers),
-                organization: {
-                    id: Equal(orgId),
-                }
-            },
-        });
-
         this.server
-            .to(this.roomPrefix + orgId)
-            .emit(WS_ACTIVE_USER_EMIT.JOIN, userEntityList);
+            .to(this.roomPrefix + roomId)
+            .emit(WS_ACTIVE_USER_EMIT.JOIN, payload);
     }
 
 
@@ -92,19 +72,17 @@ export class ActiveUsersGateway implements OnGatewayInit, OnGatewayConnection, O
 
     async handleConnection(client: Socket)
     {
-        const { user } = client.data;
+        const user: AccessTokenData = client.data.user;
         const roomName = this.roomPrefix + user.orgId;
         client.join(roomName);
         client.data.room = roomName;
 
-        await this._service.repository.update(user.sub, {
-            lastActiveAt: new Date(),
+        const aaaaaaaaaaa = await this._service.repository.update(user.sub, {
+            isOnline: true,
+            lastOnlineAt: new Date(),
         });
-
-        await this._redisStorage.redisClient
-            .sadd(this.keyName(user.orgId), user.sub);
-
-        await this.emitActiveUsers(user.orgId);
+        console.log('aaaaaaaaaaa', aaaaaaaaaaa);
+        this.emitActiveUser(aaaaaaaaaaa, user.orgId);
 
         this.logger.log(`Connected: { user: id-${user.sub} } && { room: ${roomName} }`);
     }
@@ -112,16 +90,14 @@ export class ActiveUsersGateway implements OnGatewayInit, OnGatewayConnection, O
 
     async handleDisconnect(client: Socket)
     {
-        const { user, room } = client.data;
+        const { user, room }: { user: AccessTokenData, room: string; } = client.data;
 
-        await this._service.repository.update(user.sub, {
-            lastActiveAt: new Date(),
+        const bbbbbbbbb = await this._service.repository.update(user.sub, {
+            isOnline: false,
+            lastOnlineAt: new Date(),
         });
-
-        await this._redisStorage.redisClient
-            .srem(this.keyName(user.orgId), user.sub);
-
-        this.emitActiveUsers(user.orgId);
+        console.log('bbbbbbbbb', bbbbbbbbb);
+        this.emitActiveUser(bbbbbbbbb, user.orgId);
 
         this.logger.log(`Disconnected: { user: id-${user.sub} } && { room: ${room} }`);
     }
