162bcebf6423b2dfde852d6c2768a129f845a7a2
Azizbek Iskandar <a7i7bek@gmail.com>
2025-04-17 15:23:39 +0500

    profile change password is fixed

--- src/users/utils/change-password.util.ts
+++ src/users/utils/change-password.util.ts
@@ -1,4 +1,4 @@
-import { NotFoundException } from '@nestjs/common';
+import { BadRequestException } from '@nestjs/common';
 import { ActiveUserData } from 'src/iam/interfaces/active-user-data.interface';
 import { UserChangePasswordDto } from '../dto/user-change-password.dto';
 import { UsersService } from '../users.service';
@@ -11,23 +11,20 @@ export async function changePasswordUtil
         activeUser: ActiveUserData,
     )
 {
-    const hashedPassword = await service.hashingService.hash(dto.currentPassword);
-
-
     const entity = await service.findOne(
         {
             where: {
                 id: activeUser.sub,
-                password: hashedPassword,
             }
         },
         activeUser,
     );
 
 
-    if (!entity)
+    const isEqual = await service.hashingService.compare(dto.currentPassword, entity.password);
+    if (!isEqual)
     {
-        throw new NotFoundException('User not found');
+        throw new BadRequestException('Current password is incorrect');
     }
 
 
